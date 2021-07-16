0177c09d5b387ae2676adfa4ef7d671a86ef2029
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2021-07-16 13:21:43 -0300

    Adicionando contador feito em html, css e js

---
+++ js/contador.js
@@ -0,0 +1,13 @@
+import Countdown from"./countdown.js";
+
+const blackFriday = new Countdown("25 November 2021 24:00:00 GMT-0300");
+const time = document.querySelectorAll("[data-time]");
+
+function showTime() {
+    time.forEach((tempo, index) => {
+        tempo.innerHTML = blackFriday.total[index]
+    });
+}
+
+showTime();
+setInterval(showTime, 1000);
