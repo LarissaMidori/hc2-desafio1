1afc84d5960dddd492bb4d3c5e30e77383d8d394
LarissaMidori <70383649+LarissaMidori@users.noreply.github.com>
2022-01-12 17:58:06 -0300

    arrumando o contador para a black friday de 2022

--- js/contador.js
+++ js/contador.js
@@ -1,6 +1,6 @@
 import Countdown from"./countdown.js";
 
-const blackFriday = new Countdown("25 November 2021 24:00:00 GMT-0300");
+const blackFriday = new Countdown("24 November 2022 24:00:00 GMT-0300");
 const time = document.querySelectorAll("[data-time]");
 
 function showTime() {
